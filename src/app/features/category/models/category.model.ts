b4a6b5ff07f7465c9a3cd945909df0733fb22ddc
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-05-23 10:56:05 -0300

    new service

---
+++ src/app/features/category/models/category.model.ts
@@ -0,0 +1,6 @@
+export interface Category
+{
+    id:string;
+    descripition:string;
+    imageURL:string;
+}
---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor() { }
+}
+
+// this.http.get("https://localhost:7148/api/v1/Categories")
